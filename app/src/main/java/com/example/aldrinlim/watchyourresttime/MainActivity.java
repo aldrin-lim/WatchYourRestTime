054540a616830ffc25281fd5d300365e16d4b0c5
Aldrin Lim <aldrinlim81@gmail.com>
2018-07-03 11:03:39 +0800

    testing timer

--- app/src/main/java/com/example/aldrinlim/watchyourresttime/MainActivity.java
+++ app/src/main/java/com/example/aldrinlim/watchyourresttime/MainActivity.java
@@ -3,17 +3,70 @@ package com.example.aldrinlim.watchyourresttime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView timer;
+    private TextView textTimer;
+    private Timer timer = new Timer();
+    private Button stop;
+    private int sec = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timer = findViewById(R.id.timer);
-//        timer.setText("00:01");
+        textTimer = findViewById(R.id.timer);
+        stop = findViewById(R.id.stopbutton);
+
+
+        // stop timer from running
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Timer Stopped", Toast.LENGTH_LONG).show();
+                timer.cancel();
+            }
+        });
+
+        startTimer();
+
+
+
+    }
+
+
+    void startTimer () {
+        timer.scheduleAtFixedRate(new TimerTask() {
+
+            public void run() {
+               runOnUiThread(new Runnable() {
+                   @Override
+                   public void run() {
+                       Log.d("debug", "seconds: " + sec);
+                       sec++;
+                       textTimer.setText(formatSecToTimer(sec));
+                   }
+               });
+            }
+        }, 1000, 1000);
+    }
+
+    String formatSecToTimer(int sec) {
+        Date d = new Date(sec * 1000L);
+        SimpleDateFormat df = new SimpleDateFormat("mm:ss"); // HH for 0-23
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String time = df.format(d);
+        return time;
     }
 }
