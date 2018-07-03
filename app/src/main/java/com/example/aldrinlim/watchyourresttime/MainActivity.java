88bdbe72596816464695deeef5dfdd235d33bee6
Aldrin Lim <aldrinlim81@gmail.com>
2018-07-03 10:22:22 +0800

    initial commit

---
+++ app/src/main/java/com/example/aldrinlim/watchyourresttime/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.aldrinlim.watchyourresttime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timer = findViewById(R.id.timer);
+//        timer.setText("00:01");
+    }
+}
